2f90c314d934d4c98925a64d83d7352b37eef881
Kenneth J <jakobsenkenneth@hotmail.no>
2026-05-13 12:26:28 +0200

    Create Oppgave315A.cs

---
+++ Emne3/Oppgave315A.cs
@@ -0,0 +1,41 @@
+﻿namespace Emne3;
+
+public class Oppgave315A
+{
+    public static void Run()
+    {
+        Console.WriteLine("Guess the number");
+      
+        var random = new Random();
+        int number = random.Next(1,100);
+        bool isCorrect = false;
+
+
+        while (isCorrect != true )
+        {
+            isCorrect = IsCorrect(number, isCorrect);
+        }
+        
+    }
+
+    private static bool IsCorrect(int number, bool isCorrect)
+    {
+        string? input = Console.ReadLine();
+        int guessedNumber = int.Parse(input);
+        if (guessedNumber>number)
+        {
+            Console.WriteLine("The number is too high");
+        }
+        else if (guessedNumber < number)
+        {
+            Console.WriteLine("The number is too low");
+        }
+        else
+        {
+            Console.WriteLine("You guessed the number!");
+            isCorrect = true;
+        }
+
+        return isCorrect;
+    }
+}

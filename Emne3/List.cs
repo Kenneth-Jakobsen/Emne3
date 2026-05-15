45281fb1279fdcb44e1dc5924e5fdee98e1ed334
Kenneth J <jakobsenkenneth@hotmail.no>
2026-05-15 10:58:06 +0200

    x

---
+++ Emne3/List.cs
@@ -0,0 +1,33 @@
+﻿namespace Emne3;
+
+public class List
+{
+    public void Run()
+    {
+        int[] numbers = { 5, 10, 32 };
+
+        //Kan endre men ikke legge til
+        numbers[0] = 100;
+        
+        List<string> navn = [];
+        
+        //Legge til
+        navn.Add("John");
+        navn.Add("Joe");
+        
+        //Slette
+        navn.Remove("Joe");
+        navn.RemoveAt(0);
+
+        //Lese
+        Console.WriteLine(navn.Count);
+        
+        //Finne første treff
+        string funnet = navn.Find(n=> n=="John");
+        Console.WriteLine(funnet);
+        
+        //Sjekke om verdien finnes
+        bool finnes = navn.Contains("John");
+    }
+    
+}
--- Emne3/Program.cs
+++ Emne3/Program.cs
@@ -1,12 +1,51 @@
 ﻿namespace Emne3;
+
 class Program
 {
     public static void Main(string[] args)
     {
+        List<string> navn = new List<string>();
 
-        var oppgave = new Oppgave315A();
-        
-        oppgave.Run();
 
+        while (true)
+        {
+            Console.WriteLine("Dette er en kontakt Liste");
+            Console.WriteLine("Skriv 1 til å legge til");
+            Console.WriteLine("Skriv 2 til å slette");
+            Console.WriteLine("Skriv 3 til å vise listen");
+            Console.WriteLine("Skriv 4 til å søkte");
+            Console.WriteLine("Skriv Q til å avslutte");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("Skriv navnet du vil legge til");
+                string mittNavn = Console.ReadLine();
+                navn.Add(mittNavn);
+            }
+            else if (input == "2")
+            {
+                navn.RemoveAt(0);
+            }
+            else if (input == "3")
+            {
+                Console.Clear();
+                foreach (var VARIABLE in navn)
+                {
+                    Console.WriteLine($"{VARIABLE}");
+                }
+            }
+            else if (input == "4")
+            {
+                Console.WriteLine("Søk etter: ");
+                string query = Console.ReadLine();
+                string found = navn.Find(n => n == query);
+                var foundIndex = navn.IndexOf(found);
+                Console.WriteLine($"{found} har index {foundIndex}");
+            }
+            else if (input == "Q")
+            {
+                break;
+            }
+        }
     }
-}                                                                                                                        
+}
